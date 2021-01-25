b08fe059f536162ba0a6092a64c995faa096c516
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-25 14:25:02 +0900

    新規投稿画面実装、保存

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,14 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    Book.create(book_params)
+    
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :story, :impression)
+  end
+
 end
