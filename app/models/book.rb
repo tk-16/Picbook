fb812c11b17c4c04301734d41f0abc82c78815a7
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-26 15:18:05 +0900

    バリデーション記載

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  validates :title, presence: true
+  validates :story, presence: true
+  validates :impression, presence: true
 end
